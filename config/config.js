55ebfdbcad0ffbaea6db34f7634b37d13c044727
zhuxintao2588 <79305471+zhuxintao2588@users.noreply.github.com>
2021-09-10 18:19:08 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const host = "https://hdtv.neu6.edu.cn"
+const host = "https://webtv-6g10tti967269c07-1254317835.ap-shanghai.app.tcloudbase.com"
 const version = "v1"
 const versionDetail = "1.1.0"
 module.exports = {
